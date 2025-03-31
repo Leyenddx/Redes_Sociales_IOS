9a01fb013b98c47ffef6c76a93c5dba8d0bdf1d2
Leyenddx <aarok_ny224@hotmail.com>
2025-03-31 10:54:36 -0600

    [AGREGAMOS]menu en la parte inferior de la aplicacion2

--- Redes_Sociales/Redes_Sociales/ui/menu_inicial/menu_aplicacion.swift
+++ Redes_Sociales/Redes_Sociales/ui/menu_inicial/menu_aplicacion.swift
@@ -5,4 +5,25 @@
 //  Created by alumno on 3/31/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct MenuNavegacion: View {
+    @Environment(ControladorAplicacion.self) var controlador
+    
+    var body: some View {
+        TabView{
+            PublicacionesGeneral()
+                .tabItem {Label( "Publicaciones", systemImage: "signpost.and.arrowtriangle.up.fill" )}
+                .badge(controlador.publicaciones.count)
+            Text("Hela desde pantalla 2")
+                .tabItem { Label( "Pantalla 2", systemImage: "square.and.arrow.up.fill" )}
+            Text("Hela desde pantalla 3")
+                .tabItem { Label( "Perfil", systemImage: "person.fill" )}
+        }
+    }
+}
+
+#Preview {
+    MenuNavegacion()
+        .environment(ControladorAplicacion())
+}
--- Redes_Sociales/Redes_Sociales/ui/publicaciones/publicaciones_general.swift
+++ Redes_Sociales/Redes_Sociales/ui/publicaciones/publicaciones_general.swift
@@ -10,27 +10,40 @@ import SwiftUI
 struct PublicacionesGeneral: View {
     @Environment(ControladorAplicacion.self) var controlador
     
+    //colores
+    let acent = Color(red: 0.67041, green: 0.78467, blue: 0.97889)
+    let onAccent = Color(red: 0.06025, green: 0.16915, blue: 0.36485)
+    
     var body: some View {
         NavigationStack{
          ScrollView{
             VStack{
+                Text("Publicaciones Generales")
+                    .foregroundColor(onAccent)
                 ForEach(controlador.publicaciones){ publicacion in
                     NavigationLink{
                         PublicacionVista()
                     } label : {
-                        HStack{
-                            Text("\(publicacion.id)")
                             VStack{
-                                Text("\(publicacion.title)")
-                                Text("\(publicacion.body)")
+                                Divider()
+                                Text("Publicacion \(publicacion.id)").foregroundColor(onAccent)
+                               
+                                VStack{
+                                    Text("Tema: \(publicacion.title)")
+                                        .foregroundColor(onAccent)
+                                    Text("\(publicacion.body)")
+                                        .foregroundColor(onAccent)
+                                }
                             }
-                        }
+                            .padding(.init(top: 10, leading: 20, bottom: 10, trailing: 20))
+                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
                     }.simultaneousGesture(TapGesture().onEnded({
                         controlador.seleccionar_publicacion(publicacion)
                     }))
                 }
             }
         }
+         .background(acent)
      }
    }
 }
