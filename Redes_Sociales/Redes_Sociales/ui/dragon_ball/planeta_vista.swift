ca1b6419dbd460c2c54ee77c88d1fd19394dd26e
Leyenddx <aarok_ny224@hotmail.com>
2025-04-28 10:48:00 -0600

    [SE AGREGO] informacion de los planetas

---
+++ Redes_Sociales/Redes_Sociales/ui/dragon_ball/planeta_vista.swift
@@ -0,0 +1,18 @@
+//
+//  planeta_vista.swift
+//  Redes_Sociales
+//
+//  Created by alumno on 4/28/25.
+//
+
+import SwiftUI
+
+struct planeta_vista: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    planeta_vista()
+}
